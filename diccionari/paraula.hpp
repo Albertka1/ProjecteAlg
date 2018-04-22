2913aaa9b87c7f55b68f8373a4c1b911b18f2f3a
David Ferrer Beltran <ferrerdavid96@gmail.com>
2018-04-22 16:04:36 +0200

    afegida constant de tamany màxim

--- diccionari/paraula.hpp
+++ diccionari/paraula.hpp
@@ -4,6 +4,7 @@
 namespace diccionari {
 	// Tipus dels nombres a utilitzar (unsigned de 64bit)
 	typedef unsigned long long paraula;
+	const paraula PARAULA_MAX = 10000000000000000000LLU;
 }
 
 #endif
