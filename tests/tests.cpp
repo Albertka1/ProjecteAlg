aac16701f2824b4136ba282ac321d6797855c5bd
David Ferrer Beltran <ferrerdavid96@gmail.com>
2018-04-26 17:08:51 +0200

    test is best

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -5,21 +5,45 @@
 #include <string>
 #include <vector>
 
-#include "diccionari.hpp"
-#include "generador.hpp"
+#include "diccionari/diccionari.hpp"
+#include "diccionari/factory.hpp"
+#include "diccionari/generador.hpp"
+#include "diccionari/search_tree.hpp"
 
-#include "cronometre.hpp"
-#include "display_tabular.hpp"
-#include "file_io.hpp"
-#include "string_cast.hpp"
+#include "utils/cronometre.hpp"
+#include "utils/display_tabular.hpp"
+#include "utils/file_io.hpp"
+#include "utils/string_cast.hpp"
 
 using namespace std;
 using namespace diccionari;
 using namespace utils;
 
 namespace tests_diccionari {
-	int test1(int d, int t, float p) {
-		cout << "\tTEST DICCIONARIS 1" << endl;
+	int minitest(int n) {
+		cout << "\tMINITEST Diccionari(" << n << ")" << endl;
+
+		vector<paraula> vdicc = { 2, 4, 6, 8, 10, 1, 13, 15 };
+		vector<bool> trobats = vector<bool>();
+		Diccionari *dicc = diccionari::factory(n, vdicc);
+		if (dicc == NULL) return -1;
+
+		cout << "Diccionari = " << string(*dicc) << endl;
+
+		cout << "existeix(3): " << to_string(dicc->existeix(3)) << endl;
+		cout << "existeix(8): " << to_string(dicc->existeix(8)) << endl;
+
+		trobats = dicc->existeix_lot({ 3, 8 });
+		cout << "existeix_lot(3): " << to_string<bool>(trobats[0]) << endl;
+		cout << "existeix_lot(8): " << to_string<bool>(trobats[1]) << endl;
+
+		delete dicc;
+		cout << endl;
+		return 0;
+	}
+
+	int existeix(int n, int d, int t, float p) {
+		cout << "\tTEST Diccionari(" << n << ")->existeix" << endl;
 		cout << "Tamany diccionari:\t" << d << endl;
 		cout << "Tamany test:\t\t" << d*t << endl;
 		cout << "Proporcio elements:\t" << p << endl;
@@ -30,7 +54,7 @@ namespace tests_diccionari {
 
 		motorAleatori.llavor(time(NULL));
 		vdicc = genera_diccionari(d);
-		dicc = diccionari::factory(2, vdicc);
+		dicc = diccionari::factory(n, vdicc);
 		entrada = genera_text(d*t, p, vdicc);
 
 		for (paraula par : entrada)
@@ -41,8 +65,34 @@ namespace tests_diccionari {
 		return 0;
 	}
 
-	int test2(int d, int t, float p) {
-		cout << "\tTEST DICCIONARIS 2" << endl;
+	int existeix_lot(int n, int d, int t, float p) {
+		cout << "\tTEST Diccionari(" << n << ")->existeix_lot" << endl;
+		cout << "Tamany diccionari:\t" << d << endl;
+		cout << "Tamany test:\t\t" << d * t << endl;
+		cout << "Proporcio elements:\t" << p << endl;
+
+		Diccionari* dicc;
+		vector<paraula> vdicc = vector<paraula>();
+		vector<paraula> entrada = vector<paraula>();
+		vector<bool> trobats = vector<bool>();
+
+		motorAleatori.llavor(time(NULL));
+		vdicc = genera_diccionari(d);
+		dicc = diccionari::factory(n, vdicc);
+		entrada = genera_text(d*t, p, vdicc);
+
+		trobats = dicc->existeix_lot(entrada);
+
+		for (unsigned i = 0; i < entrada.size(); ++i)
+			cout << entrada[i] << " " << (trobats[i] ? "FOUND" : "NOT FOUND") << endl;
+
+		cout << endl;
+		delete dicc;
+		return 0;
+	}
+
+	int comparativa(int n, int d, int t, float p) {
+		cout << "\tCOMPARATIVA Diccionari(" << n << ")" << endl;
 		cout << "Tamany diccionari:\t" << d << endl;
 		cout << "Tamany test:\t\t" << d*t << endl;
 		cout << "Proporcio elements:\t" << p << endl;
@@ -57,7 +107,7 @@ namespace tests_diccionari {
 
 		motorAleatori.llavor(time(NULL));
 		vdicc = genera_diccionari(d);
-		dicc = diccionari::factory(2, vdicc);
+		dicc = diccionari::factory(n, vdicc);
 		entrada = genera_text(d*t, p, vdicc);
 		trobats.reserve(entrada.size()); // Assegura temps constant en .push_back
 		if (dicc == NULL) return -1;
@@ -127,14 +177,40 @@ namespace tests_utils {
 	}
 }
 
-int main(int argc, char** argv) {    
-	// if (tests_diccionari::test1(8, 5, 0.1f) < 0) return -1;
-	if (tests_diccionari::test2(1000, 100, 0.01f) < 0) return -2;
-	if (tests_diccionari::test2(1000, 100, 0.05f) < 0) return -2;
-	if (tests_diccionari::test2(30000, 100, 0.01f) < 0) return -2;
-	if (tests_diccionari::test2(30000, 100, 0.05f) < 0) return -2;
-	// if (tests_utils::cronometre() < 0) return -3;
-	// if (tests_utils::disp_t    () < 0) return -4;
+int main(int argc, char** argv) {
+	int i = -1;
+
+	//if (tests_diccionari::minitest(DictType::tCercaSequencial) < 0) return i; --i;
+	//if (tests_diccionari::existeix(DictType::tCercaSequencial, 8, 5, 0.1f) < 0) return i; --i;
+	//if (tests_diccionari::existeix_lot(DictType::tCercaSequencial, 8, 5, 0.1f) < 0) return i; --i;
+
+	//if (tests_diccionari::minitest(DictType::tSetFind) < 0) return i; --i;
+	//if (tests_diccionari::existeix(DictType::tSetFind, 8, 5, 0.1f) < 0) return i; --i;
+	//if (tests_diccionari::existeix_lot(DictType::tSetFind, 8, 5, 0.1f) < 0) return i; --i;
+
+	//if (tests_diccionari::minitest(DictType::tUSetFind) < 0) return i; --i;
+	//if (tests_diccionari::existeix(DictType::tUSetFind, 8, 5, 0.1f) < 0) return i; --i;
+	//if (tests_diccionari::existeix_lot(DictType::tUSetFind, 8, 5, 0.1f) < 0) return i; --i;
+
+	//if (tests_diccionari::minitest(DictType::tBTree) < 0) return i; --i;
+	//if (tests_diccionari::existeix(DictType::tBTree, 8, 5, 0.1f) < 0) return i; --i;
+	//if (tests_diccionari::existeix_lot(DictType::tBTree, 8, 5, 0.1f) < 0) return i; --i;
+
+	if (tests_diccionari::minitest(DictType::tBinarySearchTree) < 0) return i; --i;
+	if (tests_diccionari::existeix(DictType::tBinarySearchTree, 8, 5, 0.1f) < 0) return i; --i;
+	if (tests_diccionari::existeix_lot(DictType::tBinarySearchTree, 8, 5, 0.1f) < 0) return i; --i;
+
+	//if (tests_diccionari::minitest(DictType::tTreap) < 0) return i; --i;
+	//if (tests_diccionari::existeix(DictType::tTreap, 8, 5, 0.1f) < 0) return i; --i;
+	//if (tests_diccionari::existeix_lot(DictType::tTreap, 8, 5, 0.1f) < 0) return i; --i;
+
+	//if (tests_diccionari::comparativa(DictType::tSetFind, 1000, 100, 0.01f) < 0) return i; --i;
+	//if (tests_diccionari::comparativa(DictType::tSetFind, 1000, 100, 0.05f) < 0) return i; --i;
+	//if (tests_diccionari::comparativa(DictType::tSetFind, 30000, 100, 0.01f) < 0) return i; --i;
+	//if (tests_diccionari::comparativa(DictType::tSetFind, 30000, 100, 0.05f) < 0) return i; --i;
+
+	//if (tests_utils::cronometre() < 0) return i; --i;
+	//if (tests_utils::disp_t    () < 0) return i; --i;
 
 #ifdef _MSC_VER
 	cin.ignore();
