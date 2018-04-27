682d22011cac43aa0d91da285b22e00f257e011f
DavidFerrerBeltran <ferrerdavid96@gmail.com>
2018-04-27 11:25:30 +0200

    Reestructurat per llegiblitat

--- diccionari/factory.hpp
+++ diccionari/factory.hpp
@@ -2,20 +2,36 @@
 #define diccionari_factory_hpp
 
 #include "diccionari.hpp"
+#include "contenidors_stl.hpp"
+#include "filtres.hpp"
 #include "search_tree.hpp"
+#include "taules_hash.hpp"
 
 namespace diccionari {
-	enum DictType {tCercaSequencial = 1, tSetFind, tUSetFind, tBTree, tBinarySearchTree, tTreap};
+	enum DictType {
+		tCercaSequencial=1, tSetFind, tUSetFind, // contenidors_stl
+		// filtres
+		tBTree, tBST, tTreap, // search_tree
+		// taules_hash
+	};
 
 	Diccionari* factory(int type, const std::vector<paraula>& pars) {
 		Diccionari* d;
 		switch (type) {
-		case  1: d = new CercaSequencial(pars); break;
-		case  2: d = new SetFind(pars); break;
-		case  3: d = new USetFind(pars); break;
-		// case  4: d = new BTree(pars); break;
-		case  5: d = new BinarySearchTree(pars); break;
-		// case  6: new Treap(pars); break;
+		// contenidors_stl
+		case  tCercaSequencial: d = new CercaSequencial(pars); break;
+		case  tSetFind: d = new SetFind(pars); break;
+		case  tUSetFind: d = new USetFind(pars); break;
+		
+		// filtres
+		
+		// search_tree
+		// case  tBTree: d = new BTree(pars); break;
+		case  tBST: d = new BinarySearchTree(pars); break;
+		// case  tTreap: new Treap(pars); break;
+		
+		// taules_hash
+		
 		default: d = NULL;
 		}
 
