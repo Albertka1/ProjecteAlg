bb583300d1a751d38c709a67231873462c38fbfc
DavidFerrerBeltran <ferrerdavid96@gmail.com>
2018-04-27 11:22:54 +0200

    Creats fitxers per als algoritmes de filtres i els de taules de hash

---
+++ diccionari/filtres.hpp
@@ -0,0 +1,10 @@
+#ifndef diccionari_filtres_hpp
+#define diccionari_filtres_hpp
+
+#include "diccionari.hpp"
+
+namespace diccionari {
+
+}
+
+#endif
---
+++ diccionari/taules_hash.hpp
@@ -0,0 +1,10 @@
+#ifndef diccionari_taules_hash_hpp
+#define diccionari_taules_hash_hpp
+
+#include "diccionari.hpp"
+
+namespace diccionari {
+
+}
+
+#endif
