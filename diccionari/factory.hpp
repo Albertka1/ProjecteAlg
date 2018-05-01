0ad820aaa3d5e8a19d1ce69130b75d9348c534b1
David Ferrer Beltran <ferrerdavid96@gmail.com>
2018-05-01 19:45:08 +0200

    Factory accepta arguments adicionals

--- diccionari/factory.hpp
+++ diccionari/factory.hpp
@@ -14,33 +14,32 @@ namespace diccionari {
 		tCercaSequencial=1, tSetFind, tUSetFind, // contenidors_stl
 		// filtres
 		tBST, tTreap, // search_tree
-		// taules_hash
-		Murmur, SHA, DJB2, MD5, xxHash, FNV,
-		LinearProbbing
+		Murmur, SHA, DJB2, MD5, xxHash, FNV, LinearProbbing // taules_hash
 	};
 
-	Diccionari* factory(int type, const std::vector<paraula>& pars) {
+	template <class ... Args>
+	Diccionari* factory(int type, const std::vector<paraula>& pars, Args ... args) {
 		Diccionari* d;
 		switch (type) {
 		// contenidors_stl
-		case  tCercaSequencial: d = new CercaSequencial(pars); break;
-		case  tSetFind: d = new SetFind(pars); break;
-		case  tUSetFind: d = new USetFind(pars); break;
+		case  tCercaSequencial: d = new CercaSequencial(pars, args ...); break;
+		case  tSetFind: d = new SetFind(pars, args ...); break;
+		case  tUSetFind: d = new USetFind(pars, args ...); break;
 		
 		// filtres
 		
 		// search_tree
-		case  tBST: d = new BinarySearchTree(pars); break;
-		// case  tTreap: new Treap(pars); break;
+		case  tBST: d = new BinarySearchTree(pars, args ...); break;
+		// case  tTreap: new Treap(pars, args ...); break;
 		
 		// taules_hash
-		case Murmur:	d = new HashTableDefault(pars);	std::cout << "Murmur" << std::endl;						break;
-		case SHA:		d = new HashTableSHA(pars);		std::cout << "SHA amb shrink per tall" << std::endl;	break;
-		case DJB2:		d = new HashTableDJB2(pars);	std::cout << "DJB2" << std::endl;						break;
-		case MD5:		d = new HashTableMD5(pars);		std::cout << "MD5" << std::endl;						break;
-		case xxHash:	d = new HashTablexxHash(pars);	std::cout << "xxHash" << std::endl;						break;
-		case FNV:		d = new HashTableFNV(pars);		std::cout << "FNV" << std::endl;						break;
-		case LinearProbbing: d = new HashSet(pars);		std::cout << "Linear probbing" << std::endl;			break;
+		case Murmur:	d = new HashTableDefault(pars, args ...);	std::cout << "Murmur" << std::endl;						break;
+		case SHA:		d = new HashTableSHA(pars, args ...);		std::cout << "SHA amb shrink per tall" << std::endl;	break;
+		case DJB2:		d = new HashTableDJB2(pars, args ...);	std::cout << "DJB2" << std::endl;						break;
+		case MD5:		d = new HashTableMD5(pars, args ...);		std::cout << "MD5" << std::endl;						break;
+		case xxHash:	d = new HashTablexxHash(pars, args ...);	std::cout << "xxHash" << std::endl;						break;
+		case FNV:		d = new HashTableFNV(pars, args ...);		std::cout << "FNV" << std::endl;						break;
+		case LinearProbbing: d = new HashSet(pars, args ...);		std::cout << "Linear probbing" << std::endl;			break;
 		default: d = NULL;
 		}
                 
