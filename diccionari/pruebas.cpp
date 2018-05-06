a96a2e04d157db88a0de2a998195fc3f6e39a4da
David Ferrer Beltran <ferrerdavid96@gmail.com>
2018-05-06 14:02:32 +0200

    reescrits includes i using

--- diccionari/pruebas.cpp
+++ diccionari/pruebas.cpp
@@ -1,8 +1,5 @@
 #include <iostream>
-#include <cstdlib>
 #include <string>
-#include <vector>
-#include <stdio.h>
 
 
 #include "md5.hpp"
@@ -10,15 +7,15 @@
 
 
  
-using std::cout; using std::endl;
+using std;
  
 int main(int argc, char *argv[])
 {
-    std::string md5_value = md5("grape");
-    std::string md5_value2 = md5(md5_value);
+    string md5_value = md5("grape");
+    string md5_value2 = md5(md5_value);
     
-    std::string sha256_value = sha256("grape");
-    std::string sha256_value2 = sha256(sha256_value);
+    string sha256_value = sha256("grape");
+    string sha256_value2 = sha256(sha256_value);
     
     
     cout << "md5 (1) of 'grape': " << md5_value << endl;
@@ -26,5 +23,6 @@ int main(int argc, char *argv[])
     
     cout << "sha256 (1) of 'grape': " << sha256_value << endl;
     cout << "sha256 (2) of 'grape': " << sha256_value2 << endl;
+
     return 0;
 }
