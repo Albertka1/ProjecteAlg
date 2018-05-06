16d7dc512679aa559904c997c364a5505056f174
David Ferrer Beltran <ferrerdavid96@gmail.com>
2018-05-06 14:41:55 +0200

    Eliminat warning sprintf

--- diccionari/sha256.cpp
+++ diccionari/sha256.cpp
@@ -1,6 +1,10 @@
 #include <cstring>
 #include <fstream>
 #include "sha256.hpp"
+
+#ifdef _MSC_VER
+#pragma warning(disable:4996)
+#endif
  
 const unsigned int SHA256::sha256_k[64] = //UL = uint32
             {0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
