43e9ec97fa6243ca73d289b5e359d00c1f42ee55
Nhok95 <enrique_1095@hotmail.es>
2018-05-09 17:17:04 +0200

    factory amb filtres

--- diccionari/factory.hpp
+++ diccionari/factory.hpp
@@ -5,14 +5,14 @@
 
 #include "diccionari.hpp"
 #include "contenidors_stl.hpp"
-//#include "filtres.hpp"
+#include "filtres.hpp"
 #include "search_tree.hpp"
-#include "taules_hash.hpp"
+//#include "taules_hash.hpp"
 
 namespace diccionari {
 	enum DictType {
 		tCercaSequencial=1, tSetFind, tUSetFind, // contenidors_stl
-		// filtres
+		tSimpleBloom, tCountingBloom, tQuotient,// filtres
 		tBST, tTreap, // search_tree
 		Murmur, SHA, DJB2, MD5, xxHash, FNV, LinearProbbing, SeparateChaining // taules_hash
 	};
@@ -20,7 +20,7 @@ namespace diccionari {
 	template <class ... Args>
 	Diccionari* factory(int type, const std::vector<paraula>& pars, Args ... args) {
 		Diccionari* d;
-		int grillHashing = 2; // (1: Linear Probing, 2: Double Hashing)
+		int grillHashing = 2;
 		switch (type) {
 		// contenidors_stl
 		case  tCercaSequencial:	d = new CercaSequencial(pars, args ...); break;
@@ -28,21 +28,25 @@ namespace diccionari {
 		case  tUSetFind:		d = new USetFind(pars, args ...); break;
 		
 		// filtres
-		
+		case tSimpleBloom: 		d = new SimpleBloom(pars); break;
+		case tCountingBloom: 	d = new CountingBloom(pars); break;
+		//case tQuotient:			d = new Quotient(pars); break;
+		/*
 		// search_tree
 		case  tBST:				d = new BinarySearchTree(pars, args ...); break;
 		case  tTreap:			d = new Treap(pars, args ...); break;
 		
-		// taules_hash 
+		// taules_hash (1: Linear Probing, 2: Double Hashing)
 		case Murmur:			d = new HashTableDefault(pars, args ...);	std::cout << "Murmur" << std::endl;						break;
 		case SHA:				d = new HashTableSHA(pars, args ...);		std::cout << "SHA amb shrink per tall" << std::endl;	break;
-		case DJB2:				d = new HashTableDJB2(pars, args ...);		std::cout << "DJB2" << std::endl;						break;
+		case DJB2:				d = new HashTableDJB2(pars, args ...);	std::cout << "DJB2" << std::endl;						    break;
 		case MD5:				d = new HashTableMD5(pars, args ...);		std::cout << "MD5" << std::endl;						break;
 		case xxHash:			d = new HashTablexxHash(pars, args ...);	std::cout << "xxHash" << std::endl;						break;
 		case FNV:				d = new HashTableFNV(pars, args ...);		std::cout << "FNV" << std::endl;						break;
-		case LinearProbbing:	d = new HashSet(pars, grillHashing);		std::cout << (grillHashing == 1 ? "Linear probbing" : "Double Hashing") << std::endl;	break;
-		case SeparateChaining:	d = new HashSetSeparateChaining(pars);		std::cout << "Separate Chaining" << std::endl;			break;
+		case LinearProbbing:	d = new HashSet(pars, 2);		std::cout << "Linear probbing" << std::endl;			            break;
+		case SeparateChaining:  d = new HashSetSeparateChaining(pars); std::cout << "Separate Chaining" << std::endl;               break;
 		default:				d = NULL;
+		*/
 		}
                 
 		return d;
