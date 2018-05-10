a07984d51329e5edb47230ddad8cf53862b2718f
Nhok95 <enrique_1095@hotmail.es>
2018-05-10 15:30:15 +0200

    parametros iniciales quotient ok

--- diccionari/filtres.hpp
+++ diccionari/filtres.hpp
@@ -93,7 +93,7 @@ namespace diccionari {
 				pars = std::vector<paraula>(v);
 				n = v.size();
 				m = round(-(n*log(p)) / (log(2)*log(2)));  //m > k*n 
-				k = ceil(m/n*log(2));  //ceil
+				k = ceil(m/n*log(2));
 				cout << "el numero N es: " << n << endl << endl;
 				cout << "el numero M es: " << m << endl << endl;
 				cout << "el numero K es: " << k << endl << endl;
@@ -173,7 +173,7 @@ namespace diccionari {
 				pars = std::vector<paraula>(v);
 				n = v.size();
 				m = round(-(n*log(p)) / (log(2)*log(2)));  //m > k*n 
-				k = ceil(m/n*log(2));  //ceil 
+				k = ceil(m/n*log(2));   
                 setZeros();
                 for (int i = 0; i < k; i++) Counting(v, i); 
             }
@@ -199,22 +199,22 @@ namespace diccionari {
 			}
 			
 	};
-	/*
+	
 	class Quotient : public Diccionari { //deberia ser lento (permite deletes, coste espacial bajo)
 		private:
 			struct quotient_slot {
 				quotient_slot(): fr(-1), is_occupied(false), is_continuation(false), is_shifted(false){}
-				unsigned long fr; //remainder
+				long fr; //remainder
 				bool is_occupied;
 				bool is_continuation;
 				bool is_shifted;
 			};
 			
 			std::vector<paraula> pars;
-			std::vector<fr> quotient;
+			std::vector<quotient_slot> quotient;
 			unsigned int m;   //tamany del hash table
-			unsigned int p = 32;  //fingersprint size
-			unsigned int r = 24;  //least significants bits 
+			unsigned int p = 16;  //fingersprint size
+			unsigned int r;  //least significants bits 
 			 
 			//usare murmurhash (unicamente)
 			
@@ -230,10 +230,10 @@ namespace diccionari {
 			//each bucket contains 3 bits, all 0 at th begining
 			//is_occupied, is_continuation, is_shifted
 			
-			unsigned long Murmur_hash(paraula p) const {
+			unsigned long Murmur_hash(paraula par) const {
 				uint32_t seed = 95;
-				uint32_t *hash_otpt[2] = {0};
-				const unsigned long long *key = &p;
+				uint32_t *hash_otpt[1] = {0};
+				const unsigned long long *key = &par;
 
 				MurmurHash3_x86_32(key, sizeof(paraula), seed, hash_otpt);
 				
@@ -242,13 +242,16 @@ namespace diccionari {
 				ss << *hash_otpt;
 				ss >> hex >>res;
 				
-				return (unsigned long)res%m;
+				int v = 32-p;
+				int murmur_mod = (int)pow(2.0, v);
+				
+				return (unsigned long)res/murmur_mod;
 			}
 			
 			int search_cluster(const int index) {
 				int i = index;
 				bool found = false;
-				while (!found and i < m.size()) {
+				while (!found and i < m) {
 					if (quotient[i].fr == -1) found = true; //busquem espai buit
 					i++;
 				}
@@ -256,9 +259,13 @@ namespace diccionari {
 			}
 			
 			void add_element(const unsigned long f) {
-				int mod = pow(2.0, r)
-				int remainder = f % mod; //fr
-				int qindex = floor(f / mod); //fq
+				int mod = (int)pow(2.0, r);
+				unsigned long remainder = f % mod; //fr
+				unsigned long qindex = floor(f / mod); //fq
+				
+				cout << "mod es: " << mod << endl;
+				cout << "remainder es: " << remainder << endl;
+				cout << "qindex es: " << qindex << endl << endl;
 				
 				quotient_slot canonical = quotient[qindex];
 				
@@ -266,6 +273,7 @@ namespace diccionari {
 					quotient[qindex].is_occupied = true;
 					quotient[qindex].fr = remainder;
 				}
+				/*
 				else if (!canonical.is_occupied and canonical.is_continuation and canonical.is_shifted) { //0;1;1;
 					quotient[qindex].is_occupied = true;
 					quotient[qindex+1].is_shifted = true;
@@ -275,7 +283,7 @@ namespace diccionari {
 				else if (canonical.is_occupied and !quotient[qindex].is_continuation and !quotient[qindex].is_shifted) { //1;0;0;
 					if (canonical.fr < remainder) {
 						quotient[qindex+1].is_continuation = true;
-						quotient[qindex+1].is_
+						//quotient[qindex+1].is_
 					}
 					else if (canonical.fr > remainder) {
 						int last_index = search_cluster(qindex);
@@ -286,20 +294,56 @@ namespace diccionari {
 						quotient[qindex].is_occupied = true;
 						quotient[qindex].fr = remainder;
 					}
-				}
-				else if (!)
+				}*/
+				//else if (!)
 			}
 			
 			
 		public:
-			/*Quotient(const std::vector<paraula>& v) : Diccionari() {
-				unsigned int q = p-r;
+			Quotient(const std::vector<paraula>& v) : Diccionari() {
+				//unsigned int q = p-r;
+				unsigned int n = v.size();
+				unsigned int q = log2(n)+1;
+				r = p-q;
 				pars = std::vector<paraula>(v);
 				m = (unsigned int)pow(2.0,q);
-				for (paraula p: v) {
-					unsigned long f = Murmur_hash(p);
+				
+				cout << "el numero N es: " << v.size() << endl << endl;
+				cout << "el numero P es: " << p << endl << endl;
+				cout << "el numero R es: " << r << endl << endl;
+				cout << "el numero Q es: " << q << endl << endl;
+				cout << "el numero M es: " << m << endl << endl;
+				
+				quotient = vector<quotient_slot>(m);
+				
+				/*cout << "ANTES DE LA TORMENTA" << endl;
+				for (quotient_slot qs: quotient) {
+					cout << "Slot " << qs.fr << endl; 
+					cout << "Is_occupied: " << qs.is_occupied << endl;
+					cout << "Is_continuation: " << qs.is_continuation << endl;
+					cout << "Is_shifted: " << qs.is_shifted << endl << endl;
+				}*/
+				
+				cout << endl << endl;
+				/*for (paraula p1: v) {
+					unsigned long f = Murmur_hash(p1);
+					cout << "la palabra '" << p1 << "' tiene como valor hash: " << f << endl;
 					add_element(f);
+					cout << endl;
+				}*/
+				/*
+				cout << endl << "DESPUES DE LA TORMENTA" << endl;
+				
+				for (int i = 0; i < m; i++) {
+					cout << "Slot " << i << endl; 
+					cout << "Remainder " << quotient[i].fr << endl; 
+					cout << "Is_occupied: " << quotient[i].is_occupied << endl;
+					cout << "Is_continuation: " << quotient[i].is_continuation << endl;
+					cout << "Is_shifted: " << quotient[i].is_shifted << endl << endl;
 				}
+				*/
+				
+				
 				
 			}
 			
@@ -310,7 +354,7 @@ namespace diccionari {
 				
                 return conte;
             }
-	};*/
+	};
 
 }
 
